69b9bb369c312a76ee81fb51e880b579dd640772
Piotr <krzywusiakk@gmail.com>
2019-10-07 14:28:59 +0200

    refactored code

--- src/main/java/pl/krzywyyy/barter/security/WebSecurityConfig.java
+++ src/main/java/pl/krzywyyy/barter/security/WebSecurityConfig.java
@@ -15,46 +15,42 @@ import pl.krzywyyy.barter.service.UserService;
 
 
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter
-{
-	private static final String SIGN_UP_URL = "/users/register";
-	private final UserService userService;
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	public WebSecurityConfig(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder)
-	{
-		this.userService = userService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-	
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception{
-		httpSecurity.cors().and().csrf().disable().authorizeRequests()
-				.antMatchers(HttpMethod.POST,SIGN_UP_URL).permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.addFilter(getJWTAuthenticationFilter())
-				.addFilter(new Authorization(authenticationManager()))
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	}
-	
-	private AuthenticationFilter getJWTAuthenticationFilter() throws Exception
-	{
-		final AuthenticationFilter filter = new AuthenticationFilter(userService,authenticationManager());
-		filter.setFilterProcessesUrl("/users/login");
-		return filter;
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception
-	{
-		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-	}
-	
-	@Bean
-	CorsConfigurationSource corsConfigurationSource(){
-	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	source.registerCorsConfiguration("/**",new CorsConfiguration().applyPermitDefaultValues());
-	return source;
-	}
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String SIGN_UP_URL = "/users/register";
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(getJWTAuthenticationFilter())
+                .addFilter(new Authorization(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    private AuthenticationFilter getJWTAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(userService, authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
