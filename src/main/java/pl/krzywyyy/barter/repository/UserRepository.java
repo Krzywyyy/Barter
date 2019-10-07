4471c3157fb999e6ec39ade88bbf11e357686ea7
Piotr <krzywusiakk@gmail.com>
2019-10-07 14:27:53 +0200

    fixed annotation

--- src/main/java/pl/krzywyyy/barter/repository/UserRepository.java
+++ src/main/java/pl/krzywyyy/barter/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package pl.krzywyyy.barter.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import pl.krzywyyy.barter.model.User;
 
-@Service
+@Repository
 public interface UserRepository extends JpaRepository<User,Integer>
 {
 	User findByLogin(String login);
