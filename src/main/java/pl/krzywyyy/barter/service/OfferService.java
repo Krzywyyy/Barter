cf7911c4b003597e06348ac417c2346666ee2798
Piotr <krzywusiakk@gmail.com>
2019-10-17 13:06:32 +0200

    extracted crudinterface from service interfaces

--- src/main/java/pl/krzywyyy/barter/service/OfferService.java
+++ src/main/java/pl/krzywyyy/barter/service/OfferService.java
@@ -4,14 +4,6 @@ import pl.krzywyyy.barter.exception.ObjectNotExistsException;
 import pl.krzywyyy.barter.exception.OfferAlreadyConsideredException;
 import pl.krzywyyy.barter.model.dto.OfferDTO;
 
-public interface OfferService {
-    Iterable<OfferDTO> findAll();
-
-    OfferDTO find(int offerId) throws ObjectNotExistsException;
-
-    void delete(int offerId) throws ObjectNotExistsException;
-
-    OfferDTO save(OfferDTO offerDTO);
-
+public interface OfferService extends CrudInterface<OfferDTO> {
     OfferDTO consider(int offerId, boolean accepted) throws ObjectNotExistsException, OfferAlreadyConsideredException;
 }
--- src/main/java/pl/krzywyyy/barter/service/ProductService.java
+++ src/main/java/pl/krzywyyy/barter/service/ProductService.java
@@ -3,14 +3,6 @@ package pl.krzywyyy.barter.service;
 import pl.krzywyyy.barter.exception.ObjectNotExistsException;
 import pl.krzywyyy.barter.model.dto.ProductDTO;
 
-public interface ProductService {
-    Iterable<ProductDTO> findAll();
-
-    ProductDTO find(int productId) throws ObjectNotExistsException;
-
-    ProductDTO save(ProductDTO productDTO);
-
-    void delete(int productId) throws ObjectNotExistsException;
-
+public interface ProductService extends CrudInterface<ProductDTO> {
     ProductDTO update(int productId, ProductDTO newProduct) throws ObjectNotExistsException;
 }
