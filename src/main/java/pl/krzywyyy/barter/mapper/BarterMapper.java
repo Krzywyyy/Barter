7174bc04e2ed252cd25bfaf5169853c01e4bb7b7
Piotr <krzywusiakk@gmail.com>
2019-10-17 08:11:55 +0200

    added interface for mapper

--- src/main/java/pl/krzywyyy/barter/mapper/BarterMapper.java
+++ src/main/java/pl/krzywyyy/barter/mapper/BarterMapper.java
@@ -1,21 +1,7 @@
 package pl.krzywyyy.barter.mapper;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.lang.reflect.Type;
 
-@Component
-public class BarterMapper {
-    private final ModelMapper modelMapper;
-
-    @Autowired
-    public BarterMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
-
-    public <V> V map(Object object, Type clazz) {
-        return modelMapper.map(object, clazz);
-    }
+public interface BarterMapper {
+    <V> V map(Object object, Type clazz);
 }
