878e423ac4d14107dd0b75ca56d84b7aeae47f6c
Piotr <krzywusiakk@gmail.com>
2019-11-04 14:04:12 +0100

    properties classes for page and images

---
+++ src/main/java/pl/krzywyyy/barter/utils/properties/ProductImagesProperties.java
@@ -0,0 +1,6 @@
+package pl.krzywyyy.barter.utils.properties;
+
+public final class ProductImagesProperties {
+    public final static String FILE_DIRECTORY = "/home/krzywyyy/Dokumenty/BarterImages/";
+    public final static String NO_IMAGE = FILE_DIRECTORY + "no_image.png";
+}
