556abec3f6a2f81b8279b087d3e2e4d8dfd0feb3
Piotr <krzywusiakk@gmail.com>
2019-11-07 10:56:54 +0100

    added findalluserproducts method and removed unnecesaries

--- src/main/java/pl/krzywyyy/barter/products/ProductRepository.java
+++ src/main/java/pl/krzywyyy/barter/products/ProductRepository.java
@@ -2,12 +2,11 @@ package pl.krzywyyy.barter.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import pl.krzywyyy.barter.utils.enums.ProductCategories;
-import pl.krzywyyy.barter.utils.enums.Specializations;
+import pl.krzywyyy.barter.users.User;
+
+import java.util.List;
 
 @Service
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    Iterable<Product> findByCategory(ProductCategories category);
-
-    Iterable<Product> findBySpecialization(Specializations specialization);
+    List<Product> findAllByUser(User user);
 }
