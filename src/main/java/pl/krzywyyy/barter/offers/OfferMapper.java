f061f40b0d0a168728b4d51b6361ed1755499756
Piotr <krzywusiakk@gmail.com>
2019-11-19 10:53:17 +0100

    mapper

--- src/main/java/pl/krzywyyy/barter/offers/OfferMapper.java
+++ src/main/java/pl/krzywyyy/barter/offers/OfferMapper.java
@@ -5,18 +5,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import pl.krzywyyy.barter.products.ProductRepository;
+import pl.krzywyyy.barter.users.UserRepository;
 
-@Mapper(componentModel = "spring", uses = ProductRepository.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
+@Mapper(componentModel = "spring", uses = {ProductRepository.class, UserRepository.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface OfferMapper {
     @Mappings({
-            @Mapping(source = "offeredProduct.id", target = "offeredProductId"),
-            @Mapping(source = "aimedProduct.id", target = "aimedProductId")
+            @Mapping(source = "product.id", target = "productId"),
+            @Mapping(source = "offerer.id", target = "offererId")
     })
     OfferDTO offerToOfferDTO(Offer offer);
 
     @Mappings({
-            @Mapping(source = "offeredProductId", target = "offeredProduct"),
-            @Mapping(source = "aimedProductId", target = "aimedProduct")
+            @Mapping(source = "productId", target = "product"),
+            @Mapping(source = "offererId", target = "offerer")
     })
     Offer offerDTOToOffer(OfferDTO productDTO);
 }
