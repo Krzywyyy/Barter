8d7f1b68896c30501af5ec7ca65fa071dab74eab
Piotr <krzywusiakk@gmail.com>
2019-11-26 23:39:19 +0100

    added title to offer model

--- src/main/java/pl/krzywyyy/barter/offers/OfferDTO.java
+++ src/main/java/pl/krzywyyy/barter/offers/OfferDTO.java
@@ -7,6 +7,7 @@ import java.util.Date;
 @Data
 public class OfferDTO {
     private int id;
+    private String title;
     private String message;
     private int productId;
     private int offererId;
